276e4481813e2b637d5899ef0f5533507eb58fdc
Kelin Field <kfield111@hotmail.com>
2020-09-02 04:04:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
-# require libraries/modules here
+require 'Nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css(li.project.grid_4).each do |project|
+    projects[project] = {}
+  end
+
+
 end
